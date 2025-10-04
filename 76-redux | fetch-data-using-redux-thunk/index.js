55bf4286c33aa0a07e7bf9e6f9c463f9cab07818
tahasin-oyshik <tahasin.oyshik@gmail.com>
2025-10-04 18:09:41 +0600

    asdf

--- 76-redux | fetch-data-using-redux-thunk/index.js
+++ 76-redux | fetch-data-using-redux-thunk/index.js
@@ -2,3 +2,91 @@
 // api url - https://jsonplaceholder.typicode.com/todos
 // middleware - redux thunk
 // axios api
+
+import axios from 'axios';
+import { applyMiddleware, legacy_createStore as createStore } from 'redux';
+import { thunk } from 'redux-thunk';
+
+// constants
+const GET_TODOS_REQUEST = 'GET_TODOS_REQUEST';
+const GET_TODOS_SUCCESS = 'GET_TODOS_SUCCESS';
+const GET_TODOS_FAILED = 'GET_TODOS_FAILED';
+const API_URL = 'https://jsonplaceholder.typicode.com/todos';
+
+// states
+const initialTodosState = {
+  todos: [],
+  isLoading: false,
+  error: null,
+};
+
+// actions
+const getTodosRequest = () => {
+  return {
+    type: GET_TODOS_REQUEST,
+  };
+};
+const getTodosSuccess = (todos) => {
+  return {
+    type: GET_TODOS_SUCCESS,
+    payload: todos,
+  };
+};
+const getTodosFailed = (error) => {
+  return {
+    type: GET_TODOS_FAILED,
+    payload: error,
+  };
+};
+
+// reducers
+const todosReducer = (state = initialTodosState, action) => {
+  switch (action.type) {
+    case GET_TODOS_REQUEST:
+      return {
+        ...state,
+        isLoading: true,
+      };
+    case GET_TODOS_SUCCESS:
+      return {
+        ...state,
+        isLoading: false,
+        todos: action.payload,
+      };
+    case GET_TODOS_FAILED:
+      return {
+        ...state,
+        isLoading: false,
+        error: action.payload,
+      };
+    default:
+      return state;
+  }
+};
+
+// async action creator
+const fetchData = () => {
+  return (dispatch) => {
+    dispatch(getTodosRequest());
+    axios
+      .get(API_URL)
+      .then((response) => {
+        const todos = response.data;
+        const titles = todos.map((todo) => todo.title);
+        dispatch(getTodosSuccess(titles));
+      })
+      .catch((error) => {
+        const errorMessage = error.message;
+        dispatch(getTodosFailed(errorMessage));
+      });
+  };
+};
+
+// store
+const store = createStore(todosReducer, applyMiddleware(thunk));
+
+store.subscribe(() => {
+  console.log(store.getState());
+});
+
+store.dispatch(fetchData());
