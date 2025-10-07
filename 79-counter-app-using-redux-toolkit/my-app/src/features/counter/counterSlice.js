aeb1a1e50e891712f1cf6c72dfe6a9e767a99fc0
tahasin-oyshik <tahasin.oyshik@gmail.com>
2025-10-07 23:54:20 +0600

    fetch using redux toolkit

---
+++ 79-counter-app-using-redux-toolkit/my-app/src/features/counter/counterSlice.js
@@ -0,0 +1,26 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export const counterSlice = createSlice({
+  name: 'counter',
+  initialState: { count: 0 },
+  reducers: {
+    increment: (state) => {
+      state.count = state.count + 1;
+    },
+    decrement: (state) => {
+      state.count = state.count - 1;
+    },
+    reset: (state) => {
+      state.count = 0;
+    },
+    increaseByAmount: (state, action) => {
+      state.count = state.count + action.payload;
+    },
+  },
+});
+
+// exporting action creator
+export const { increment, decrement, reset, increaseByAmount } = counterSlice.actions;
+
+// exporting reducer
+export default counterSlice.reducer;
--- 79-counter-app-using-redux-toolkit/my-app/src/store.js
+++
@@ -1,7 +0,0 @@
-import { applyMiddleware, legacy_createStore as createStore } from 'redux';
-import { thunk } from 'redux-thunk';
-import todosReducer from './services/reducers/todosReducer.js';
-
-const store = createStore(todosReducer, applyMiddleware(thunk));
-
-export default store;
